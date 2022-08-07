71e21f1de8b15973f6b3a757e51611bcab07b9fa
JohnWong <john.wong@egusi.com.hk>
2022-08-07 21:59:44 +0800

    create module override price

---
+++ override_price_calculation/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "override_price_calculation",
+    'version': '1.0',
+    'depends': ['base'.'studio_customization'],
+    'author': "Techable",
+    'category': 'Transfer',
+    'description': "",
+
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'active': False,
+    # data files always loaded at installation
+    'data': [],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+}
---
+++ override_price_calculation/override_price_calculation.py
@@ -0,0 +1,23 @@
+from odoo import fields, models, api
+
+# _logger = logging.getLogger(__name__)
+
+
+class Override_Price_Calculation(models.Model):
+    
+    _inherit = 'sales.order.line'
+
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id', 'x_studio_weight')
+        def _compute_amount(self):
+            res = super(SalesOrderLine, self)._compute_amount()
+            for line in self:
+#                 price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+                price = line.price_unit * line.x_studio_weight
+                taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
+                line.update({
+                    'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+                    'price_total': taxes['total_included'],
+                    'price_subtotal': taxes['total_excluded'],
+                })
+                if self.env.context.get('import_file', False) and not self.env.user.user_has_groups('account.group_account_manager'):
+                    line.tax_id.invalidate_cache(['invoice_repartition_line_ids'], [line.tax_id.id])
