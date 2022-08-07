80448bf0c6d61bc70da928b5a02507a5ec563237
JohnWong <john.wong@egusi.com.hk>
2022-08-08 00:17:46 +0800

    update calculation method

--- override_price_calculation/override_price_calculation.py
+++ override_price_calculation/override_price_calculation.py
@@ -12,8 +12,8 @@ class Override_Price_Calculation(models.Model):
         res = super(Override_Price_Calculation, self)._compute_amount()
         for line in self:
 #           price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-            price = line.price_unit * line.x_studio_weight
-            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
+            price = line.price_unit * line.x_studio_weight * (1 - (line.discount or 0.0) / 100.0)
+            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, product=line.product_id, partner=line.order_id.partner_shipping_id)
             line.update({
                 'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
                 'price_total': taxes['total_included'],
