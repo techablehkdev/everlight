256388a50915b210ad1dc0ed63ea6246d2fdc397
JohnWong <john.wong@egusi.com.hk>
2022-08-08 02:10:53 +0800

    add tax cal

---
+++ override_price_calculation/override_tax_calculation_sales.py
@@ -0,0 +1,20 @@
+from odoo import models, api
+
+class Override_Tax_Calculation_Sales(models.Model):
+    
+    _inherit = 'sale.order'
+
+    @api.depends('order_line.tax_id', 'order_line.price_unit', 'amount_total', 'amount_untaxed')
+    def _compute_tax_totals_json(self):
+        res = super(Override_Tax_Calculation_Sales, self)._compute_tax_totals_json()
+        def compute_taxes(order_line):
+            price = order_line.price_unit * (1 - (order_line.discount or 0.0) / 100.0)
+            order = order_line.order_id
+            return order_line.tax_id._origin.compute_all(price, order.currency_id, order_line.product_uom_qty, product=order_line.product_id, partner=order.partner_shipping_id)
+
+        account_move = self.env['account.move']
+        for order in self:
+            tax_lines_data = account_move._prepare_tax_lines_data_for_totals_from_object(order.order_line, compute_taxes)
+            tax_totals = account_move._get_tax_totals(order.partner_id, tax_lines_data, order.amount_total, order.amount_untaxed, order.currency_id)
+            order.tax_totals_json = json.dumps(tax_totals)
+        return res
