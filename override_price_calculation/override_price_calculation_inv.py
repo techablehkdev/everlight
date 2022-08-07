b51c3caa17487a8b7078c58b8c1acaeb8f6b28a3
JohnWong <john.wong@egusi.com.hk>
2022-08-08 06:52:13 +0800

    fix

--- override_price_calculation/override_price_calculation_inv.py
+++ override_price_calculation/override_price_calculation_inv.py
@@ -38,6 +38,85 @@ class Override_Price_Calculation_Inv(models.Model):
         if currency:
             res = {k: currency.round(v) for k, v in res.items()}
         return res
+    
+    @api.model
+    def _get_fields_onchange_balance_model(self, quantity, discount, amount_currency, move_type, currency, taxes, price_subtotal, force_computation=False):
+        ''' This method is used to recompute the values of 'quantity', 'discount', 'price_unit' due to a change made
+        in some accounting fields such as 'balance'.
+
+        This method is a bit complex as we need to handle some special cases.
+        For example, setting a positive balance with a 100% discount.
+
+        :param quantity:        The current quantity.
+        :param discount:        The current discount.
+        :param amount_currency: The new balance in line's currency.
+        :param move_type:       The type of the move.
+        :param currency:        The currency.
+        :param taxes:           The applied taxes.
+        :param price_subtotal:  The price_subtotal.
+        :return:                A dictionary containing 'quantity', 'discount', 'price_unit'.
+        '''
+        if move_type in self.move_id.get_outbound_types():
+            sign = 1
+        elif move_type in self.move_id.get_inbound_types():
+            sign = -1
+        else:
+            sign = 1
+        amount_currency *= sign
+
+        # Avoid rounding issue when dealing with price included taxes. For example, when the price_unit is 2300.0 and
+        # a 5.5% price included tax is applied on it, a balance of 2300.0 / 1.055 = 2180.094 ~ 2180.09 is computed.
+        # However, when triggering the inverse, 2180.09 + (2180.09 * 0.055) = 2180.09 + 119.90 = 2299.99 is computed.
+        # To avoid that, set the price_subtotal at the balance if the difference between them looks like a rounding
+        # issue.
+        if not force_computation and currency.is_zero(amount_currency - price_subtotal):
+            return {}
+
+        taxes = taxes.flatten_taxes_hierarchy()
+        if taxes and any(tax.price_include for tax in taxes):
+            # Inverse taxes. E.g:
+            #
+            # Price Unit    | Taxes         | Originator Tax    |Price Subtotal     | Price Total
+            # -----------------------------------------------------------------------------------
+            # 110           | 10% incl, 5%  |                   | 100               | 115
+            # 10            |               | 10% incl          | 10                | 10
+            # 5             |               | 5%                | 5                 | 5
+            #
+            # When setting the balance to -200, the expected result is:
+            #
+            # Price Unit    | Taxes         | Originator Tax    |Price Subtotal     | Price Total
+            # -----------------------------------------------------------------------------------
+            # 220           | 10% incl, 5%  |                   | 200               | 230
+            # 20            |               | 10% incl          | 20                | 20
+            # 10            |               | 5%                | 10                | 10
+            force_sign = -1 if move_type in ('out_invoice', 'in_refund', 'out_receipt') else 1
+            taxes_res = taxes._origin.with_context(force_sign=force_sign).compute_all(amount_currency, currency=currency, handle_price_include=False)
+            for tax_res in taxes_res['taxes']:
+                tax = self.env['account.tax'].browse(tax_res['id'])
+                if tax.price_include:
+                    amount_currency += tax_res['amount']
+
+        discount_factor = 1 - (discount / 100.0)
+        if amount_currency and discount_factor:
+            # discount != 100%
+            vals = {
+                'quantity': quantity or 1.0,
+                'price_unit': amount_currency / discount_factor / (self.x_studio_weight or 1.0),
+            }
+        elif amount_currency and not discount_factor:
+            # discount == 100%
+            vals = {
+                'quantity': quantity or 1.0,
+                'discount': 0.0,
+                'price_unit': amount_currency / (self.x_studio_weight or 1.0),
+            }
+        elif not discount_factor:
+            # balance of line is 0, but discount  == 100% so we display the normal unit_price
+            vals = {}
+        else:
+            # balance is 0, so unit price is 0 as well
+            vals = {'price_unit': 0.0}
+        return vals
 
 
 class Override_Tax_Calculation_Inv(models.Model):
