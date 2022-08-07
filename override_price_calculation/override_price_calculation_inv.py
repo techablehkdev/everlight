2dc6cd31fdbee329d3e92780329700beaa055242
JohnWong <john.wong@egusi.com.hk>
2022-08-08 03:13:15 +0800

    fix inv

--- override_price_calculation/override_price_calculation_inv.py
+++ override_price_calculation/override_price_calculation_inv.py
@@ -2,21 +2,33 @@ from odoo import models, api
 
 class Override_Price_Calculation_Inv(models.Model):
     
-    _inherit = 'account.move'
-
+    _inherit = 'account.move.line'
+        
     @api.model
-    def _get_price_total_and_subtotal_model(self, price_unit, discount, currency, product, partner, taxes, move_type, x_studio_weight):
+    def _get_price_total_and_subtotal_model(self, price_unit, quantity, discount, currency, product, partner, taxes, move_type):
+#         res = super(Override_Price_Calculation_Inv, self)._get_price_total_and_subtotal_model()
+        ''' This method is used to compute 'price_total' & 'price_subtotal'.
 
-        res = super(Override_Price_Calculation_Inv, self)._get_price_total_and_subtotal_model()
-        
+        :param price_unit:  The current price unit.
+        :param quantity:    The current quantity.
+        :param discount:    The current discount.
+        :param currency:    The line's currency.
+        :param product:     The line's product.
+        :param partner:     The line's partner.
+        :param taxes:       The applied taxes.
+        :param move_type:   The type of the move.
+        :return:            A dictionary containing 'price_subtotal' & 'price_total'.
+        '''
+        res = {}
 
-        quantity = x_studio_weight
+        # Compute 'price_subtotal'.
         line_discount_price_unit = price_unit * (1 - (discount / 100.0))
-        subtotal = quantity * line_discount_price_unit
-        
+        subtotal = self.x_studio_weight * line_discount_price_unit
+
+        # Compute 'price_total'.
         if taxes:
             taxes_res = taxes._origin.with_context(force_sign=1).compute_all(line_discount_price_unit,
-                quantity=quantity, currency=currency, product=product, partner=partner, is_refund=move_type in ('out_refund', 'in_refund'))
+                quantity=self.x_studio_weight, currency=currency, product=product, partner=partner, is_refund=move_type in ('out_refund', 'in_refund'))
             res['price_subtotal'] = taxes_res['total_excluded']
             res['price_total'] = taxes_res['total_included']
         else:
