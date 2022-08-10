b2ac1c7d2390f2431285a2badfeea88bb99a6fed
JohnWong <john.wong@egusi.com.hk>
2022-08-11 06:02:00 +0800

    bug fix

--- override_price_calculation/override_price_calculation_inv.py
+++ override_price_calculation/override_price_calculation_inv.py
@@ -41,7 +41,6 @@ class AccountMoveLine(models.Model):
         :param move_type:   The type of the move.
         :return:            A dictionary containing 'price_subtotal' & 'price_total'.
         '''
-        res = {}
 
         # Compute 'price_subtotal'.
         line_discount_price_unit = ((self.x_studio_price*self.x_studio_weight)/self.quantity) * (1 - (discount / 100.0))
