cb52f7da80a721994aff7a8bd0130218c449643f
JohnWong <john.wong@egusi.com.hk>
2022-08-11 06:10:07 +0800

    test

--- override_price_calculation/override_price_calculation_inv.py
+++ override_price_calculation/override_price_calculation_inv.py
@@ -17,7 +17,7 @@ class AccountMoveLine(models.Model):
     def _get_price_total_and_subtotal(self, price_unit=None, quantity=None, discount=None, currency=None, product=None, partner=None, taxes=None, move_type=None):
         self.ensure_one()
         return self._get_price_total_and_subtotal_model(
-            price_unit=self.price_unit if price_unit is None else price_unit,
+            price_unit=((self.x_studio_price*self.x_studio_weight)/self.quantity) if price_unit is None else price_unit,
             quantity=self.quantity if quantity is None else quantity,
             discount=self.discount if discount is None else discount,
             currency=self.currency_id if currency is None else currency,
@@ -27,35 +27,35 @@ class AccountMoveLine(models.Model):
             move_type=self.move_id.move_type if move_type is None else move_type,
         )
         
-    @api.model
-    def _get_price_total_and_subtotal_model(self, price_unit, quantity, discount, currency, product, partner, taxes, move_type):
-        ''' This method is used to compute 'price_total' & 'price_subtotal'.
+    # @api.model
+    # def _get_price_total_and_subtotal_model(self, price_unit, quantity, discount, currency, product, partner, taxes, move_type):
+    #     ''' This method is used to compute 'price_total' & 'price_subtotal'.
 
-        :param price_unit:  The current price unit.
-        :param quantity:    The current quantity.
-        :param discount:    The current discount.
-        :param currency:    The line's currency.
-        :param product:     The line's product.
-        :param partner:     The line's partner.
-        :param taxes:       The applied taxes.
-        :param move_type:   The type of the move.
-        :return:            A dictionary containing 'price_subtotal' & 'price_total'.
-        '''
-        res = {}
+    #     :param price_unit:  The current price unit.
+    #     :param quantity:    The current quantity.
+    #     :param discount:    The current discount.
+    #     :param currency:    The line's currency.
+    #     :param product:     The line's product.
+    #     :param partner:     The line's partner.
+    #     :param taxes:       The applied taxes.
+    #     :param move_type:   The type of the move.
+    #     :return:            A dictionary containing 'price_subtotal' & 'price_total'.
+    #     '''
+    #     res = {}
 
-        # Compute 'price_subtotal'.
-        line_discount_price_unit = ((self.x_studio_price*self.x_studio_weight)/quantity) * (1 - (discount / 100.0))
-        subtotal = quantity * line_discount_price_unit
+    #     # Compute 'price_subtotal'.
+    #     line_discount_price_unit = price_unit * (1 - (discount / 100.0))
+    #     subtotal = quantity * line_discount_price_unit
 
-        # Compute 'price_total'.
-        if taxes:
-            taxes_res = taxes._origin.with_context(force_sign=1).compute_all(line_discount_price_unit,
-                quantity=quantity, currency=currency, product=product, partner=partner, is_refund=move_type in ('out_refund', 'in_refund'))
-            res['price_subtotal'] = taxes_res['total_excluded']
-            res['price_total'] = taxes_res['total_included']
-        else:
-            res['price_total'] = res['price_subtotal'] = subtotal
-        #In case of multi currency, round before it's use for computing debit credit
-        if currency:
-            res = {k: currency.round(v) for k, v in res.items()}
-        return res
+    #     # Compute 'price_total'.
+    #     if taxes:
+    #         taxes_res = taxes._origin.with_context(force_sign=1).compute_all(line_discount_price_unit,
+    #             quantity=quantity, currency=currency, product=product, partner=partner, is_refund=move_type in ('out_refund', 'in_refund'))
+    #         res['price_subtotal'] = taxes_res['total_excluded']
+    #         res['price_total'] = taxes_res['total_included']
+    #     else:
+    #         res['price_total'] = res['price_subtotal'] = subtotal
+    #     #In case of multi currency, round before it's use for computing debit credit
+    #     if currency:
+    #         res = {k: currency.round(v) for k, v in res.items()}
+    #     return res
