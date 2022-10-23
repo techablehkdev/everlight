6c014e1c8175aae979e3f40d636a15c870eb6a0c
JohnWong <john.wong@egusi.com.hk>
2022-10-24 01:35:30 +0800

    fix item select bug

--- override_price_calculation/override_price_calculation_inv.py
+++ override_price_calculation/override_price_calculation_inv.py
@@ -41,18 +41,19 @@ class SaleOrderLine(models.Model):
         Compute the amounts of the SO line.
         """
         for line in self:
-            price = line.x_studio_price * self.x_studio_weight  * (1 - (line.discount or 0.0) / 100.0)
+            price = line.x_studio_price * line.x_studio_weight  * (1 - (line.discount or 0.0) / 100.0)
             taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
             line.update({
                 'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
                 'price_total': taxes['total_included'],
                 'price_subtotal': taxes['total_excluded'],
             })
+            if line.product_uom_qty !=False:
+                if (line.x_studio_price*line.x_studio_weight /line.product_uom_qty) != False:
+                    line.update({ 'price_unit':  line.x_studio_price*line.x_studio_weight/line.product_uom_qty})
+                    
             if self.env.context.get('import_file', False) and not self.env.user.user_has_groups('account.group_account_manager'):
                 line.tax_id.invalidate_cache(['invoice_repartition_line_ids'], [line.tax_id.id])
-            if self.product_uom_qty !=False:
-                if (self.x_studio_price*self.x_studio_weight /self.product_uom_qty) != False:
-                    line.update({ 'price_unit':  self.x_studio_price*self.x_studio_weight/self.product_uom_qty})
                     
                     
     def _update_taxes(self):
